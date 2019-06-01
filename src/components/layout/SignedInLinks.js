242eeb91b8d9cc93a4e46205c422bc7c551f63a1
Nat Grant <email.natgrant@gmail.com>
2019-06-01 13:34:59 +1200

    user can logout

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
 
-const SignedInLinks = () => {
+import { signOut } from "../../store/actions/authActions";
+
+const SignedInLinks = props => {
   return (
     <ul className="right">
       <li>
         <NavLink to="/create">New Project</NavLink>
       </li>
       <li>
-        <NavLink to="/">Log Out</NavLink>
+        <a href="" onClick={props.signOut}>
+          Log Out
+        </a>
       </li>
       <li>
         <NavLink to="/" className="btn btn-floating pink lighten-1">
@@ -19,4 +24,13 @@ const SignedInLinks = () => {
   );
 };
 
-export default SignedInLinks;
+const mapDispatchToProps = dispatch => {
+  return {
+    signOut: () => dispatch(signOut())
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SignedInLinks);
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -16,6 +16,10 @@ const authReducer = (state = initState, action) => {
         ...state,
         authError: null
       };
+    case "SIGNOUT_SUCCESS":
+      console.log("Signed out");
+      return state;
+
     default:
       return state;
   }
